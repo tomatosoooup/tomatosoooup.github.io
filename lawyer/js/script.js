2cbdd2d7b9d0df89de74bd14fcddf9ad5c7f0949
tomatosoooup <you@example.com>
2022-05-19 13:15:07 +0300

    massive changes/ added slider

---
+++ lawyer/js/script.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function(){
+    $('.carousel_inner').slick({
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        speed: 800,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+    });
+  });
