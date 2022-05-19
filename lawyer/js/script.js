748eab68ed34f6c5d42f103f81be5433d90074b2
tomatosoooup <you@example.com>
2022-05-19 14:24:09 +0300

    added scroll times jquery

--- lawyer/js/script.js
+++ lawyer/js/script.js
@@ -8,4 +8,14 @@
         autoplaySpeed: 5000,
         pauseOnHover:true,
     });
+    $("#change-slider").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+		//забираем идентификатор бока с атрибута href
+		let id  = $(this).attr('href'),
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		//анимируем переход на расстояние - top за 400 мс
+		$('body,html').animate({scrollTop: top}, 400);
+	});
   });
