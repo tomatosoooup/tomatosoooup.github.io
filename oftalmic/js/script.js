d21618c893601539a3bc81bb05ab6326c246d2aa
tomatosoooup <you@example.com>
2022-06-11 11:47:29 +0300

    new project

---
+++ oftalmic/js/script.js
@@ -0,0 +1,24 @@
+﻿// window.onresize = function (event) {
+//   let height = document.documentElement.clientHeight; //высота экрана
+//   let width = document.documentElement.clientWidth; //ширина экрана
+//   let text = document.getElementsByClassName("fio"); // ФИО доктора
+//   let doctor = document.getElementById("doctor"); //фото доктора
+//   //Работа с размером фото при уменьшении высоты экрана
+//   if (height > 500) {
+//     doctor.style.height = "600px";
+//   }
+//   if (height < 500) {
+//     doctor.style.height = "400px";
+//   }
+//   if (height < 400) {
+//     doctor.style.height = "300px";
+//   }
+//   if (height < 300) {
+//     doctor.style.height = "200px";
+//     doctor.style.width = "300px";
+//   }
+//   if (height < 200) {
+//     doctor.style.height = "100px";
+//     doctor.style.width = "200px";
+//   }
+// };
