a6481afac0ec70b19c06840a751cb3be83a6779c
tomatosoooup <you@example.com>
2022-06-14 12:45:35 +0300

    slider

--- oftalmic/js/script.js
+++ oftalmic/js/script.js
@@ -1,4 +1,26 @@
-﻿$(document).ready(function () {
+﻿const widthOnStart = window.innerWidth;
+if (widthOnStart <= "768") {
+  $(".license-container").removeClass("col-lg-5 col-md-5 col-tb-5 col-sm-5");
+  $(".licenses-wrapper").addClass("carousel");
+  $(".licenses-inner").addClass("carousel_inner");
+  $(".license-container").addClass("carousel_item");
+  let allowed = true;
+  if (widthOnStart > "600") {
+    allowed = true;
+  } else {
+    allowed = false;
+  }
+  $(".carousel_inner").slick({
+    slidesToShow: 1,
+    speed: 800,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    pauseOnHover: true,
+    variableWidth: allowed,
+  });
+}
+$(document).ready(function () {
   $(".menu-burger").click(function (event) {
     $(".menu-burger, .menu").toggleClass("active");
     $("body").toggleClass("lock");
@@ -6,36 +28,40 @@
 
   $(window).resize(function (event) {
     let width = window.innerWidth;
-    let licenses_wrapper = document.querySelector(".licenses-wrapper");
     if (width > "768") {
-      let html = [
-        '<div class="licenses-inner col-lg-10 col-md-10 col-tb-10 col-sm-10">',
-        '<div class="license-container">',
-        '<img src="images/sertificate.png" alt="">',
-        "</div>",
-        '<div class="license-container">',
-        '<img src="images/sertificate.png" alt="">',
-        "</div>",
-        '<div class="license-container">',
-        '<img src="images/sertificate.png" alt="">',
-        "</div>",
-        '<div class="license-container">',
-        '<img src="images/sertificate.png" alt="">',
-        "</div>",
+      html = [
+        '<div class="licenses-inner col-lg-10 col-md-10 col-tb-9 col-sm-9">',
+        '<div class="license-container col-lg-5 col-md-5 col-tb-5 col-sm-5"><img src="images/sertificate.png" alt=""></div>',
+        '<div class="license-container col-lg-5 col-md-5 col-tb-5 col-sm-5"><img src="images/sertificate.png" alt=""></div>',
+        '<div class="license-container col-lg-5 col-md-5 col-tb-5 col-sm-5"><img src="images/sertificate.png" alt=""></div>',
+        '<div class="license-container col-lg-5 col-md-5 col-tb-5 col-sm-5"><img src="images/sertificate.png" alt=""></div>',
         "</div>",
       ].join("");
-      licenses_wrapper.innerHTML = "";
-      licenses_wrapper.innerHTML = html;
+      document.querySelector(".licenses-wrapper").innerHTML = "";
+      document.querySelector(".licenses-wrapper").innerHTML = html;
     } else {
+      let width = window.innerWidth;
+      $(".license-container").removeClass(
+        "col-lg-5 col-md-5 col-tb-5 col-sm-5"
+      );
+      $(".licenses-wrapper").addClass("carousel");
+      $(".licenses-inner").addClass("carousel_inner");
+      $(".license-container").addClass("carousel_item");
+      let allowed = true;
+      if (width > "600") {
+        allowed = true;
+      } else {
+        allowed = false;
+      }
+      $(".carousel_inner").slick({
+        slidesToShow: 1,
+        speed: 800,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnHover: true,
+        variableWidth: allowed,
+      });
     }
-  });
-  $(".carousel_inner").slick({
-    slidesToShow: 1,
-    speed: 800,
-    variableWidth: true,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    pauseOnHover: true,
   });
 });
