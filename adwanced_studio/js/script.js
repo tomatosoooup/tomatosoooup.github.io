3355b7556a485b70170aae10331f9f02c4075884
tomatosoooup <you@example.com>
2022-08-06 11:54:52 +0300

    ru lang added

--- adwanced_studio/js/script.js
+++ adwanced_studio/js/script.js
@@ -63,7 +63,7 @@ $(document).ready(function () {
       $(services_item[0])
         .find("p")
         .html(
-          "We do full site functionality, for convenient work and convenience the client"
+          "We make the full functionality of the site convenient for the client to use"
         );
 
       $(services_item[1])
@@ -168,7 +168,7 @@ $(document).ready(function () {
       $(services_item[0])
         .find("p")
         .html(
-          "Ми робимо повний функціонал сайту, для зручної роботи та зручності клієнта"
+          "Ми робимо повний функціонал сайту зручним для користування клієнта"
         );
       $(services_item[1])
         .find("p")
@@ -198,7 +198,7 @@ $(document).ready(function () {
       $(schedule_item[1])
         .find("p")
         .html(
-          "Вислуховуючи Ваші побажання, ми ставимо додаткові питання, висловлюємо своє бачення та фіксуємо підсумок"
+          "Вислуховуючи ваші побажання, ми ставимо додаткові питання, висловлюємо своє бачення та фіксуємо підсумок"
         );
       $(schedule_item[2]).find("span").html("Підписання договору");
       $(schedule_item[2])
@@ -212,7 +212,7 @@ $(document).ready(function () {
       $(schedule_item[3])
         .find("p")
         .html(
-          "Після завершення робіт ми передаємо Вам усі файли у вихідних форматах, доступи до адміністративної панелі та всі матеріали, які були розроблені"
+          "Після завершення робіт ми передаємо вам усі файли у вихідних форматах, доступи до адміністративної панелі та всі матеріали, які були розроблені"
         );
       // Конец шагов работы
       // Контактная форма
@@ -241,15 +241,92 @@ $(document).ready(function () {
 
     if (language == "#RU") {
       $(".studio__inner p").html(
-        "Створення цифрових продуктів, що трансформують бізнес"
+        "Создание цифровых продуктов, <br> которые трансформируют бизнес"
       );
       let links = $(".links");
       let all_li = links.find("a").find("li");
-      $(all_li[0]).html("Main");
-      $(all_li[1]).html("Portfolio");
-      $(all_li[2]).html("Options");
-      $(all_li[3]).html("Contacts");
-      $(all_li[4]).html("Connect with us");
+      $(all_li[0]).html("Главная");
+      $(all_li[1]).html("Портфолио");
+      $(all_li[2]).html("Услуги");
+      $(all_li[3]).html("Контакты");
+      $(all_li[4]).html("Св'язаться с нами");
+
+      $(".portfolio h4").html("Примеры готовых робот");
+      $(".portfolio h2").html("Наше портфолио");
+      $(".portfolio h3").html(
+        "Над созданием каждого сайта мы подходим индивидуально"
+      );
+      $(".options span").html("Разработка");
+      $(".options h2").html("Услуги");
+      $(".options p").html("Виберите нужную услугу и мы начнём работать");
+      $(".feedback a").html("Св'язаться с нами");
+      // Услуги
+      let services = $(".options-2");
+      let services_item = services.find("div.option");
+      $(services_item[0]).find("h3").html("Веб разработка");
+      $(services_item[0])
+        .find("p")
+        .html(
+          "Мы делаем полный функционал сайта удобным для пользования клиента"
+        );
+      $(services_item[1])
+        .find("p")
+        .html(
+          "Разрабатываем дизайн, делая сайт приятным для глаз та желанием вернуться снова"
+        );
+
+      $(services_item[2]).find("h3").html("Моб. программы");
+      $(services_item[2])
+        .find("p")
+        .html(
+          "Делаем удобный концепт программы для удобности пользования на телефоне"
+        );
+      // Конец услуг
+      // Шаги работы
+      $(".steps h4").html("Как мы работаем");
+      $(".steps h2").html("Последовательность работы");
+      let schedule = $(".schedule-wrapper");
+      let schedule_item = schedule.find("div.text");
+      $(schedule_item[0]).find("span").html("Св'язь с нами");
+      $(schedule_item[0])
+        .find("p")
+        .html(
+          "Вы св'язываетесь с нами по телефону или оставляет заявку, после чего мы перезваниваем"
+        );
+      $(schedule_item[1]).find("span").html("Обсуждение и составление Т.З");
+      $(schedule_item[1])
+        .find("p")
+        .html(
+          "Выслушивая ваши пожелания, ми задаём дополнительные вопросы, высказываем своё виденье и фіксируем итог"
+        );
+      $(schedule_item[2]).find("span").html("Подписание договора");
+      $(schedule_item[2])
+        .find("p")
+        .html(
+          "После составления технического задания мы укладываем офифиальный договор, что имеет юридическую силу"
+        );
+      $(schedule_item[3]).find("span").html("Сдача работы и передача доступов");
+      $(schedule_item[3])
+        .find("p")
+        .html(
+          "После окончания работы мы передаём вам все файлы в выходных форматах, доступы до административной панели и все материалы, которые были разработаны"
+        );
+      // Конец шагов работы
+      // Контактная форма
+      $(".application-text h2").html("Контакты");
+      $(".application-text h3").html("Предложите свои идеи");
+      $(".application-form h3").html("Напиши нам");
+      $(".form")
+        .find("label.col-lg-3")
+        .find("input")
+        .attr("placeholder", "Имя");
+      $(".form")
+        .find("label.col-lg-8")
+        .find("input")
+        .attr("placeholder", "Електронная почта");
+      $("textarea").attr("placeholder", "Каким бы вы хотели видить ваш сайт ?");
+      $(".form-button").html("Отослать заказ");
+      // Конец контактной формы
 
       // Значения ссылок для опредиления языка
       let current_href = $(".menu_link").attr("href");
