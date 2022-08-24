4a425f0c4180d9c77e75d663a40b7dde3f39a449
tomatosoooup <you@example.com>
2022-08-24 21:11:19 +0300

    added preloader

--- reabilitolog/js/script.js
+++ reabilitolog/js/script.js
@@ -1,3 +1,7 @@
+$(window).on("load", function () {
+  $(".preloader").fadeOut().end().delay(400).fadeOut("slow");
+});
+
 $(document).ready(function () {
   $(window).resize(function () {
     const img = $(".take");
