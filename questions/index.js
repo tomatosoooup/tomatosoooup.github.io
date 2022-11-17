46abc23e06cfa156d3567d6873e4f874ea9f4549
tomatosoooup <v.gabinet02@icloud.com>
2022-11-17 09:57:21 +0200

    new quiz

---
+++ questions/index.js
@@ -0,0 +1,21 @@
+let ask = document.getElementById("question");
+let container = document.querySelector(".questions");
+let questions = document.querySelectorAll(".questions li");
+let values = [];
+for (let i = 0; i < questions.length; i++) {
+  values.push(questions[i].innerHTML);
+}
+
+function askMe() {
+  rand = values[(values.length * Math.random()) | 0];
+  ask.innerHTML = rand;
+  let ind = values.indexOf(rand);
+  questions.forEach((el) => {
+    if (el.innerText == String(values[ind])) {
+      el.remove();
+    }
+  });
+  if (ind > -1) {
+    values.splice(ind, 1);
+  }
+}
