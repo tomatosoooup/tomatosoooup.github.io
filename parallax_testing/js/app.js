0ed9babc7c1fd22b95e800bc182bd28a56380aff
Vasyl <v.gabinet02@icloud.com>
2023-11-17 12:31:55 +0200

    deleted not needed projects

--- parallax_testing/js/app.js
+++
@@ -1,8 +0,0 @@
-const canvas = document.getElementById("canvas_one");
-let myFluid = new Fluid(canvas);
-
-myFluid.PARAMS.emitter_size = 0;
-myFluid.PARAMS.transparent = true;
-
-myFluid.activate();
-myFluid.PARAMS.emitter_size = 0.5;
